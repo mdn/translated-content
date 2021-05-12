416964af057b95fa6d6b85903571ac368f8c20a3
hochan222 <hochan049@gmail.com>
2021-05-12 21:44:33 +0900

    [FEAT] add script file for .gitmodules

---
+++ start-server.sh
@@ -0,0 +1,10 @@
+git submodule init
+git submodule update
+
+echo 'CONTENT_TRANSLATED_ROOT=../files' >> ./content/.env
+echo 'EDITOR=code' >> ./content/.env
+
+cd content
+
+yarn install
+yarn start
