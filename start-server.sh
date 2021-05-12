a2cabce58183febc754ebf9e9da5703fc51ce70d
hochan222 <hochan049@gmail.com>
2021-05-12 21:58:56 +0900

    [FEAT] add bash for server automation

--- start-server.sh
+++ start-server.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+# Notify content repo in git local config
 git submodule init
+# Cloning content repo into translated repo
 git submodule update
 
-echo 'CONTENT_TRANSLATED_ROOT=../files' >> ./content/.env
-echo 'EDITOR=code' >> ./content/.env
+# Add .env file 
+echo 'CONTENT_TRANSLATED_ROOT=../files\nEDITOR=code' > ./content/.env
 
+# Run server
 cd content
-
 yarn install
 yarn start
