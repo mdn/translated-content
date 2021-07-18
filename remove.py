96cbc00b1df3887130844ab4e5de76df87ccc7bc
hochan222 <hochan049@gmail.com>
2021-07-18 21:47:49 +0900

    fix: remove wiki

---
+++ remove.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+import re
+
+fixes = []
+
+
+class Stop(Exception):
+    """exit early"""
+
+
+NEEDLE = """https:\/\/wiki.developer.mozilla.org"""
+
+def fix(path):
+    if len(fixes) > 15:
+        raise Stop
+
+    new_content = ""
+    with open(path) as f:
+        content = f.read()
+        p = re.compile(NEEDLE)
+        match_lists = p.findall(content)
+        for i in match_lists:
+            new_content = content.replace(i, "")
+        # if NEEDLE in content:
+        #     new_content = content.replace(NEEDLE, "")
+
+    if new_content:
+        print("CHANGE", path)
+        with open(path, "w") as f:
+            f.write(new_content)
+        fixes.append(path)
+
+
+def walk(root: Path):
+    for thing in root.iterdir():
+        if thing.is_dir():
+            walk(thing)
+        elif thing.name == "index.html":
+            fix(thing)
+
+
+try:
+    walk(Path("files/ko"))
+except Stop:
+    print("MAX REACHED!")
+print(len(fixes))
